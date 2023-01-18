de72fe03d93e3f75610d32b690839a8b21231282
Shlok Mishra <99207534+Jougan-0@users.noreply.github.com>
2023-01-18 12:52:05 +0530

    feat:Cash register

---
+++ index.js
@@ -0,0 +1,39 @@
+const billAmount=document.querySelector("#bill-amount");
+const Cashgiven=document.querySelector("#Cash-given");
+const CheckButton=document.querySelector(".check-button");
+var message=document.querySelector(".error-message");
+const NotesNumber=document.querySelectorAll(".Notes");
+var Notes=[2000,500,100,50,10,1];
+
+CheckButton.addEventListener("click",function ValidateBillAmount(){
+    message.style.display="none";
+    if(billAmount.value>=0)
+    {
+        if(Cashgiven.value>=billAmount.value){
+            var Cashleft=Cashgiven.value-billAmount.value;
+            console.log(Cashleft)
+            NotesCalculated(Cashleft);
+        }
+        else
+        DenyMessage("Do you wish to wash the plates");
+    }
+    else
+    DenyMessage("Invalid Amount entered");
+});
+
+
+function NotesCalculated(cashToBeCalculated){
+    for(var i=0;i<Notes.length;i++)
+    {
+        var NotesNeeded=Math.trunc(cashToBeCalculated/Notes[i]);
+        cashToBeCalculated%=Notes[i];
+        console.log(NotesNeeded);
+        NotesNumber[i].innerText=NotesNeeded;
+    }
+}
+
+function DenyMessage(msg)
+{
+    message.style.display="block";
+    message.innerText=msg;
+}
